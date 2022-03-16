cd0e38f8b39ece1fc62d6269106f99864783df9c
Shiv Singh <shivsingh7150@hotmail.com>
2022-03-16 13:58:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Antler - Hosting Provider & WHMCS Template</title>
+        <meta name="description" content="" />
+        <link href="img/favicon.ico" rel="shortcut icon"/>
       </Head>
 
       <main>
