1dd148e3ea4857f680d6bffdc305f71416f02d79
Shiv Singh <shivsingh7150@hotmail.com>
2022-03-16 20:02:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Js from '@components/Js'
-import { Html, Main, NextScript } from 'next/document'
+import Html from 'next/document'
 
 export default function Home() {
     return (
