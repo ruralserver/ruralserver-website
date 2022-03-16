c2b450673ef81777902c7d9f894063ee3e1b831b
Shiv Singh <shivsingh7150@hotmail.com>
2022-03-16 14:07:16 +0530

    css hunting

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,3 @@
-    import "styles/fonts/cloudicon/cloudicon.css";
-    import "styles/fonts/fontawesome/css/all.css";
-    import "styles/bootstrap.min.css";
-    import "styles/owl.carousel.min.css";
-    import "styles/swiper.min.css";
-    import "styles/animate.min.css";
-    import "styles/style.min.css";
-    import "styles/custom.css";
-
     export default function Header() {
     return (
       <>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import '@styles/globals.css'
+import "@styles/fonts/cloudicon/cloudicon.css";
+import "@styles/fonts/fontawesome/css/all.css";
+import "@styles/bootstrap.min.css";
+import "@styles/owl.carousel.min.css";
+import "@styles/swiper.min.css";
+import "@styles/animate.min.css";
+import "@styles/style.min.css";
+import "@styles/custom.css";
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
