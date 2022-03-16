a9943c5ee6c6465cc957678c82c94a409e4c3911
Shiv Singh <shivsingh7150@hotmail.com>
2022-03-16 19:58:50 +0530

    next doc

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head />
+            <body data-layout="wide" data-color="green" data-background="dark" data-font="opensans" data-header="fixed" data-textdirection="ltr">
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    )
+}
