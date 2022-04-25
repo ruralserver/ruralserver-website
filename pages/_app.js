c9bf9ead68beaecf26865a84bc12ce10e513b3ed
Shiv Singh <shivsingh7150@hotmail.com>
2022-04-25 12:01:15 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import "@styles/fonts/cloudicon/cloudicon.css";
-import "@styles/fonts/fontawesome/css/all.css";
-import "@styles/bootstrap.min.css";
-import "@styles/owl.carousel.min.css";
-import "@styles/swiper.min.css";
-import "@styles/animate.min.css";
-import "@styles/style.min.css";
-import "@styles/theme-change.css";
-import "@styles/custom.css";
+import "/styles/fonts/cloudicon/cloudicon.css";
+import "/styles/fonts/fontawesome/css/all.css";
+import "/styles/bootstrap.min.css";
+import "/styles/owl.carousel.min.css";
+import "/styles/swiper.min.css";
+import "/styles/animate.min.css";
+import "/styles/style.min.css";
+import "/styles/theme-change.css";
+import "/styles/custom.css";
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
