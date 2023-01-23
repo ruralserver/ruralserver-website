1edea32d7284b3f451e70fad170197ceae13beff
Shiv Singh <shivsingh7150@hotmail.com>
2023-01-23 14:57:31 +0530

    meta

--- pages/web-hosting/shared-linux-hosting.js
+++ pages/web-hosting/shared-linux-hosting.js
@@ -8,8 +8,8 @@ import Js from '@components/Js'
 export default function Home() {
     return (
         <>
+            <Meta />
             <Head>
-                <Meta />
             <title>Shared linux hosting</title>
             <meta name="description" content="" />
             </Head>
