48d02c7729c04265207ffa11f84ec8f278769928
Shiv Singh <shivsingh7150@hotmail.com>
2023-01-23 13:24:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Header from '@components/Header';
-import Footer from '@components/Footer';
-import Js from '@components/Js';
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import Js from '@components/Js'
 
 export default function Home() {
     return (
