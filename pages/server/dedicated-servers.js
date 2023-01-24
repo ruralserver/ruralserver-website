445702719736585cdc6bb69bbceba7d319135ab4
Shiv Singh <shivsingh7150@hotmail.com>
2023-01-24 11:19:38 +0530

    Update dedicated-servers.js

--- pages/server/dedicated-servers.js
+++ pages/server/dedicated-servers.js
@@ -5,7 +5,7 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Js from '@components/Js'
 
-export default function Home() {
+export default function DedicatedServers() {
     return (
         <>
             <Meta />
